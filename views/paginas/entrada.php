874f82c8cb31abf7c351c00a4db2105dc5a1f1f4
Claudia Tobias <113573031+Fl4utia@users.noreply.github.com>
2024-03-12 22:34:53 -0600

    Agregando directorio bienesRaicesMVC

---
+++ views/paginas/entrada.php
@@ -0,0 +1,20 @@
+
+<main class="contenedor seccion contenido-centrado">
+    <h1>Guía para la decoración de tu hogar</h1>
+
+
+    <picture>
+        <source srcset="build/img/destacada2.webp" type="image/webp">
+        <source srcset="build/img/destacada2.jpg" type="image/jpeg">
+        <img loading="lazy" src="build/img/destacada2.jpg" alt="imagen de la propiedad">
+    </picture>
+
+    <p class="informacion-meta">Escrito el: <span>20/10/2024</span> por: <span>Admin</span> </p>
+
+
+    <div class="resumen-propiedad">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi minus similique iste incidunt id eveniet earum perspiciatis esse, ea recusandae reiciendis laboriosam natus aspernatur. Similique, aperiam! Eos nisi quos culpa!</p>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, sint labore! Laudantium nisi exercitationem, cum inventore maxime alias animi expedita veritatis rerum sapiente? Repellendus vel possimus molestias repellat tempore aliquid.</p>
+    </div>
+</main>
---
+++ views/paginas/propiedades.php
@@ -0,0 +1,7 @@
+<main class="contenedor seccion">
+
+<h2>Casas y Depas en Venta</h2>
+    <?php 
+    $short = false;
+    include 'listado.php'; ?>
+</main>

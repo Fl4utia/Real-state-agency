a7ed9b50161a629db354817a132c6c84e9670dd0
Claudia Tobias <113573031+Fl4utia@users.noreply.github.com>
2024-03-17 11:10:41 -0600

    Update index.php

--- views/paginas/index.php
+++ views/paginas/index.php
@@ -52,7 +52,7 @@
             </div>
 
             <div class="texto-entrada">
-                <a href="entrada.php">
+                <a href="/entrada">
                     <h4>Terraza en el techo de tu casa</h4>
                     <p class="informacion-meta">Escrito el: <span>20/10/2021</span> por: <span>Admin</span> </p>
 
@@ -73,7 +73,7 @@
             </div>
 
             <div class="texto-entrada">
-                <a href="entrada.php">
+                <a href="/entrada">
                     <h4>Guía para la decoración de tu hogar</h4>
                     <p class="informacion-meta">Escrito el: <span>20/10/2021</span> por: <span>Admin</span> </p>
 
